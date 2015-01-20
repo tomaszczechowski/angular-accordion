7c8c3c80e086ebe6b39f1facac190819ccc08a35
Tomasz Czechowski <tomasz@czechowski.pl>
2015-01-20 20:43:07 +0100

    Updated AccordionService
    Added setter and getters

--- accordion/services/accordion.js
+++ accordion/services/accordion.js
@@ -4,8 +4,34 @@
   var app = angular.module('accordion');
 
   app.service('AccordionService', function() {
-    return {
+  	var _oneOpen 		= true
+  		, _openClass 	= 'open'
+  		, _closeClass = 'close';
 
+    return {
+    	setOneOpen: function (_value) {
+    		_oneOpen = _value;
+    	},
+
+    	getOneOpen: function () {
+    		return _oneOpen;
+    	},
+
+    	setOpenClass: function (_class) {
+    		_openClass = _class;
+    	},
+
+    	getOpenClass: function () {
+    		return _openClass;
+    	},
+
+    	setCloseClass: function (_class) {
+    		_closeClass = _class;
+    	},
+
+    	getCloseClass: function () {
+    		return _closeClass;
+    	}
     };
   });
 })();
