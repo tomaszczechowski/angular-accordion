c71c8af7f9275cdb26f0888ddda3dd63c75c1d98
Tomasz Czechowski <tomasz@czechowski.pl>
2015-01-20 20:47:33 +0100

    Updated AccordionItemService
    Added ItemModel and API to controle it

--- accordion/services/item.js
+++ accordion/services/item.js
@@ -4,8 +4,48 @@
   var app = angular.module('accordion');
 
   app.service('AccordionItemService', function() {
-    return {
+  	var _items = [];
 
+  	var ItemModel = function (options) {
+  		this._open = options.open;
+  		this._header = options.header;
+  	};
+
+  	ItemModel.prototype = {
+  		open: function () {
+  			this._open = true;
+  		},
+
+  		close: function () {
+  			this._open = false;
+  		},
+
+  		isOpen: function () {
+  			return this._open;
+  		}
+  	};
+
+    return {
+    	add: function (open, header) {
+    		var item = new ItemModel({
+    			open: open,
+    			header: header
+    		});
+
+    		_items.push(item);
+
+    		return item;
+    	},
+
+    	get: function (index) {
+    		return _items[index];
+    	},
+
+    	closeAll: function () {
+    		for (var i = 0; i < _items.length; i++) {
+    			_items[i].close();
+    		};
+    	}
     };
   });
 })();
